da87d928f22f27fcecceb409b39a53131b09b8d3
Cale Sennett <calesennett@gmail.com>
2015-03-22 10:37:46 -0500

    complete fsm implementation

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -1,13 +1,19 @@
 package fsm
 
+import (
+	"strconv"
+	"strings"
+)
+
 type StateMachine struct {
 	States      []State
 	Transitions []Transition
 }
 
 type State struct {
-	ID    int
-	Label string
+	ID      int
+	Label   string
+	Initial bool
 }
 
 type Transition struct {
@@ -18,5 +24,46 @@ type Transition struct {
 func Parse(lines []string) StateMachine {
 	states := []State{}
 	transitions := []Transition{}
+
+	for i, line := range lines {
+		if line[0:6] == "STATES" {
+			strStates := strings.Split(line, " ")[1]
+			numStates, _ := strconv.Atoi(strStates)
+			states = createStates(numStates)
+		}
+		if line[0:4] == "INIT" {
+			curLine := i
+			for isInt(lines[curLine+1]) {
+				state, _ := strconv.Atoi(lines[curLine+1])
+				states = markInitial(states, state)
+				curLine++
+			}
+		}
+	}
 	return StateMachine{states, transitions}
+}
+
+func createStates(numStates int) []State {
+	states := []State{}
+	for i := 0; i < numStates; i++ {
+		state := State{i, "", false}
+		states = append(states, state)
+	}
+	return states
+}
+
+func markInitial(states []State, initState int) []State {
+	for _, state := range states {
+		if state.ID == initState {
+			state.Initial = true
+		}
+	}
+	return states
+}
+
+func isInt(state string) bool {
+	if _, err := strconv.Atoi(state); err == nil {
+		return true
+	}
+	return false
 }
