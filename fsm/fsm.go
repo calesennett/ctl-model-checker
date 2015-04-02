449004d7b67f014bfa9c3c40f0a579326ce7454b
Cale Sennett <calesennett@gmail.com>
2015-04-02 17:44:36 -0500

    add comments on exported types

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -7,22 +7,31 @@ import (
 )
 
 type StateMachine struct {
+	// Models a finite state machine with
+	// a set of states and transitions.
 	States      []State
 	Transitions []Transition
 }
 
 type State struct {
+	// A state can be labeled with a proposition
+	// and optionally be an initial state.
 	ID      int
 	Label   string
 	Initial bool
 }
 
 type Transition struct {
+	// A transition has a from state
+	// and a to state.
 	from int
 	to   int
 }
 
 func Parse(lines []string) (StateMachine, []string) {
+	// Parse parses a .fsm file (std-in) and creates
+	// and returns a StateMachine and list of
+	// computations to be performed on the machine.
 	states := []State{}
 	transitions := []Transition{}
 	computations := []string{}
